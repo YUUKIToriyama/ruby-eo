d39d790ca6a584b68bd71f1a37336372ede6bda3
YUUKIToriyama <ytoriyama@torisfactory.com>
2019-04-24 22:52:28 +0900

    some fixes

--- ruby-eo.rb
+++ ruby-eo.rb
@@ -1,108 +1,112 @@
 class Esperanto < String
-	def is_pol?
+	def is_pol? #複数形かどうか(plural form)
 	 if self[-2] + self[-1] == "oj"
 		return true
 	 else
 		return false
 	 end
 	end
-	def is_female?
+	def is_female? #女性形かどうか
 	 if self[-3] + self[-2] + self[-1] == "ino" || self[-3] + self[-2] + self[-1] == "ina"
 		return true
 	 else
 		return false
 	 end
 	end
-	def is_noun?
+	def is_noun? #名詞かどうか
 	 if self[-1] == "o" || self[-2] == "o"
 		return true
 	 else
 		return false
 	 end
 	end
-	def is_adj?
+	def is_adj? #形容詞かどうか
 	 if self[-1] == "a" || self[-2] == "a"
 		return true
 	 else
 		return false
 	 end
 	end
-	def is_adv?
+	def is_adv? #副詞かどうか
 	 if self[-1] == "e" || self[-2] == "e"
 		return true
 	 else
 		return false
 	 end
 	end
-	def is_inf?
+	def is_inf? #動詞の原形(infinitive)
 	 if self[-1] == "i"
 	   return true
 	 else
 	   return false
 	 end
 	end
-	def is_past?
+	def is_past? #過去形かどうか
 	 if self[-2] + self[-1] == "is"
 	 	return true
 	 else
 		return false
 	 end
 	end
-	def is_present?
+	def is_present? #現在形かどうか
 	 if self[-2] + self[-1] == "as"
 		return true
 	 else
 		return false
 	 end
 	end
-	def is_future?
+	def is_future? #未来形かどうか
 	 if self[-2] + self[-1] == "os"
 		return true
 	 else
 		return false
 	 end
 	end
-	def is_imp?
+	def is_imp? #命令形(imperative)かどうか
 	 if self[-1] == "u"
 		return true
 	 else
 		return false
 	 end
 	end
-	def is_conditional?
+	def is_conditional? #叙想形かとうか
 	 if self[-2] + self[-1] == "us"
 		return true
 	 else
 		return false
 	 end
 	end
-	def is_verb?
+	def is_verb? #動詞かどうか
 	 if self.is_inf? || self.is_past? || self.is_present? || self.is_future? || self.is_imp? || self.is_conditional
 		return true
 	 else
 		return false
 	 end
 	end
-	def is_negative?
+	def is_negative? ##
 	 if self.is_verb? && self[0] + self[1] == "ne"
 		return true
 	 else
 		return false
 	 end
 	end
-	def is_opposite?
+	def is_opposite? ##反意語かどうか(接頭辞mal-を伴うかどうか)
 	 if self[0] + self[1] + self[2] == "mal"
 		return true
 	 else
 		return false
 	 end
 	end
-	def make_opposite
-	 prefix = "mal"
-	 temp = prefix + self
-	 return temp
+	def make_opposite #反意語をつくる
+	 if (!self.is_opposite?)
+		 prefix = "mal"
+		 temp = prefix + self
+		 return temp
+	 else
+		return self.sub(/^mal/, "")
+	 end
 	end
-	def make_female
+	def make_female #女性形にする
 	 if (!self.is_female?)
 		if self.is_noun?
 		 self[-1] = ''
@@ -113,6 +117,8 @@ class Esperanto < String
 		else
 		 return "#{self} is not noun or adjective"
 		end
+	 else
+		return "#{self} is already female noun or adjective"
 	 end
 	end
 	def to_s
